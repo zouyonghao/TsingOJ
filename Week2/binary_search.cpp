c604cf4af9c1292ecf8ca7ac40291bf44d69ce7c
yonghaoz1994@gmail.com <yonghaoz1994@gmail.com>
2018-06-29 09:17:58 +0800

    week2 finished.

---
+++ Week2/binary_search.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ================= 代码实现开始 =================
+
+/* 请在这里定义你需要的全局变量 */
+
+// 本函数传入数组 a 及所有询问，你需要求解所有询问并一并返回
+// n：序列 a 的长度
+// a：存储了序列 a
+// Q：询问个数
+// query：依次存储了所有询问的参数 x
+// 返回值：一个 vector<int>，依次存放各询问的答案
+vector<int> getAnswer(int n, vector<int> a, int Q, vector<int> query) {
+    vector<int> ans;
+    ans.clear();
+    sort(a.begin(), a.end());
+    for (int i = 0; i < Q; ++i) {
+        int key = query[i];
+        int l = 0, r = a.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (key > a[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        int pos = r + 1;
+        if (a[pos] < key) {
+            ans.push_back(-1);
+        } else {
+            ans.push_back(a[pos]);
+        }
+    }
+    return ans;
+}
+
+// ================= 代码实现结束 =================
+
+int main() {
+    int n, Q, tmp;
+    vector<int> a, query;
+    a.clear();
+    query.clear();
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &tmp);
+        a.push_back(tmp);
+    }
+    scanf("%d", &Q);
+    for (int i = 0; i < Q; ++i) {
+        scanf("%d", &tmp);
+        query.push_back(tmp);
+    }
+    vector<int> ans = getAnswer(n, a, Q, query);
+    for (int i = 0; i < Q; ++i)
+        printf("%d\n", ans[i]);
+    return 0;
+}
--- Week2/maxgroup.cpp
+++ Week2/maxgroup.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 /* 请在这里定义你需要的全局变量 */
 bool check(long long d, int n, int m, vector<int> & a) {
-    
+    long long sum = 0;
+    int cnt = 1;
+
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > d)
+            return false;
+        sum += a[i];
+        if (sum > d) {
+            sum = a[i];
+            cnt ++;
+        }
+    }
+
+    return cnt <= m;
 }
 
 // 将所给数组分成连续的m份，使得数字之和最大的那一份的数字之和最小
@@ -14,7 +27,22 @@ bool check(long long d, int n, int m, vector<int> & a) {
 // a：所给数组，大小为n
 // 返回值：最优方案中，数字之和最大的那一份的数字之和
 long long getAnswer(int n, int m, vector<int> a) {
-    /* 请在这里设计你的算法 */
+    long long l = 1, r = 0;
+
+    for (int i = 0; i < n; ++i) {
+        r += a[i];
+    }
+
+    while (l <= r) {
+        long long mid = (l + r) >> 1;
+        if (check(mid, n, m, a)) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    return r + 1;
 }
 
 // ================= 代码实现结束 =================
